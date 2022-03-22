51205ab981d68ba47828244f4743367d5d599fe9
Vaibhav9526 <81508584+Vaibhav9526@users.noreply.github.com>
2022-03-22 13:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,18 +16,81 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
-// STICKY NAVBAR FUNCTION -->
+//DARK - LIGHT MODE FUNCTION FOR A GOOD LOOK!!
+function changeMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+//=========NAVBAR=========
+
+(function($) { 
+  $(function() { 
+
+    //  open and close nav 
+    $('#navbar-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
 
 
-window.onscroll = function() {myFunction()};
+    // Hamburger toggle
+    $('#navbar-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+      // Close dropdown when select another dropdown
+      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
+
+
+    // Click outside the dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.navbar-dropdown').hide();
+    });
+  }); 
+})(jQuery); 
+
+
+
+/*
+Steps / article on my website:
+https://henryegloff.com/how-to-code-a-simple-dark-mode-toggle/
+
+*/
+
+function toggle_light_mode() {
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.lightMode == "dark") {
+      localStorage.lightMode = "light";
+      app.setAttribute("light-mode", "light");
   } else {
-    navbar.classList.remove("sticky");
+      localStorage.lightMode = "dark";
+      app.setAttribute("light-mode", "dark");
   }
 }
+
+window.addEventListener(
+  "storage",
+  function () {
+      if (localStorage.lightMode == "dark") {
+          app.setAttribute("light-mode", "dark");
+      } else {
+          app.setAttribute("light-mode", "light");
+      }
+  },
+  false
+);
+
+
+var app = document.getElementsByTagName("BODY")[0];
+if (localStorage.lightMode == "dark") {
+    app.setAttribute("light-mode", "dark");
+}
+
+
